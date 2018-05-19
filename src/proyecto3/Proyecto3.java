92232e45e433e5b69d7be65b1327324be7dbbc67
JonathanC3 <jonathan@jonathan-Satellite-C45-A>
2018-05-18 22:19:25 -0600

    first commit

--- src/proyecto3/Proyecto3.java
+++ src/proyecto3/Proyecto3.java
@@ -27,7 +27,7 @@ public class Proyecto3 extends Application {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                //System.out.println("Hello World!");
             }
         });
         
